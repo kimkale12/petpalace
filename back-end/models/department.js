71c645e51e0f82a3c49d5d29dc4c4e95cbb2852a
kimkale12 <whis779@naver.com>
2025-06-08 23:02:04 +0900

    fe 브랜치에서 back-end 디렉토리 제거

--- back-end/models/department.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-
-class Department extends Sequelize.Model {}
-
-module.exports = Department;
--- back-end/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const departmentRouter = require('./department');
-const paymentRouter = require('./payment');
-const router = express.Router();
-
-router.use('/departments', departmentRouter);
-router.use('/payment', paymentRouter); // '/naverpay' 대신 '/pay'
-
-module.exports = router;
